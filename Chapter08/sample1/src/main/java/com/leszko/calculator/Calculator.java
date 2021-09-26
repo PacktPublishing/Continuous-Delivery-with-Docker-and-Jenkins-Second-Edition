c0d78676759e59e97f03028f74b06dffa5de66c4
Dan Lambright <dlambrig@gmail.com>
2021-09-26 16:10:09 -0400

    calculator logic

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,7 +3,6 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/** Calculator logic */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
